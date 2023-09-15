12cfdd6e09f422099e78020b3bfbeee4857ea03e
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-09-15 17:47:43 +0500

    asdasd

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./i18n.js";
 import ThemeContextProvider from "./contexts/ThemeContextProvider.jsx";
-
-import "react-lazy-load-image-component/src/effects/blur.css";
 import CartContextProvider from "./contexts/CartContextProvider.jsx";
+
+import "react-lazy-load-image-component/src/effects/blur.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
