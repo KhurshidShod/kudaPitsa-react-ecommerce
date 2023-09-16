aacc5a0edb31b2898fc3dd486c2d7095a1c649bc
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-09-16 13:26:28 +0500

    asd

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -39,6 +39,7 @@ const Card = ({ prod }) => {
     flyingImg.classList.add('flyingImage')
     parent.appendChild(flyingImg)
     parent.style.position = 'relative'
+    parent.parentNode.parentNode.parentNode.style.overflow = 'hidden'
     
     const flyingImgPos = flyingImg.getBoundingClientRect()
     const cartBtnPos = document.querySelector('.cartBtn').getBoundingClientRect()
