f1ecd7f65f17a7cb119af1b9ecd2f99005c4ec37
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-11-26 00:31:45 +0500

    Update index.jsx

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -6,7 +6,7 @@ const Cart = () => {
       <div className="container">
         <div className={styles.cart}>
           <div className={styles.cart__products}>
-            
+            <h1>Cart is not ready yet. So sorry for this</h1>
           </div>
         </div>
       </div>
